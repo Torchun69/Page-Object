8d3fa616ea8de63805f4eddfd0fd447d05c7f34e
Torchun69 <103356396+Torchun69@users.noreply.github.com>
2022-05-23 21:49:16 +0400

    added login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,13 +9,16 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
         # реализуйте проверку на корректный url адрес
         assert True
 
     def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
         # реализуйте проверку, что есть форма логина
         assert True
 
     def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         # реализуйте проверку, что есть форма регистрации на странице
         assert True
